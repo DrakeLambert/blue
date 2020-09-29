46b62e30cda56bb04afaf19591d8d5f398e74aab
Drake Lambert <drake-lambert@outlook.com>
2020-09-28 21:20:35 -0500

    add temporary room picker select handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import RoomPicker from './Components/RoomPicker.jsx';
 import roomTypes from './ExternalData/room-types.json'
 
 const App = () => <Container className='pt-3'>
-	<RoomPicker roomTypes={roomTypes} onSelect={1} />
+	<RoomPicker roomTypes={roomTypes} onSelect={roomType => console.log(roomType)} />
 </Container>
 
 export default App
