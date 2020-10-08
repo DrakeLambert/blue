0ef42545013ac276a7fbdac18ef5d07bff32f376
Drake Lambert <17054016+DrakeLambert@users.noreply.github.com>
2020-10-07 21:13:22 -0500

    make bathroom images responsive on quote your bathroom

--- src/Pages/QuoteYourJob/Bathroom/index.jsx
+++ src/Pages/QuoteYourJob/Bathroom/index.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import typeImages from './TypeImages'
 
 export default () => <>
-	<h1>Just a heads up!</h1>
-	<h3>We renovate bathrooms that look like this...</h3>
-	<Row className='justify-content-center'>
+	<h4>Just a heads up!</h4>
+	<p>We renovate more traditional bathrooms. Like this...</p>
+	<Row className='justify-content-around mb-3 mx-n0'>
 		{typeImages.map((TypeImage, i) =>
-			<Col xs={6} key={i}>
-				{i < 2
-					? <TypeImage className='w-100 d-block mx-auto' />
-					: <TypeImage className='w-100 d-none d-sm-block mx-auto' />
-				}
+			<Col xs={5} sm={3} key={i} className={(i < 2 ? '' : 'd-none d-sm-block ') + 'px-0'}>
+				<TypeImage className='w-100' />
 			</Col>
 		).reduce((xs, x, i) => <>
 			{xs}
-			{i < 2
-				? <Col xs={2}>
-					<h3 className='text-center'>or</h3>
-				</Col>
-				: <Col xs={2} className='d-none d-sm-block'>
-					<h3 className='text-center'>or</h3>
-				</Col>
-			}
+			<Col xs='auto' className={(i < 2 ? '' : 'd-none d-sm-block ') + 'align-self-center px-0'}>
+				<h5>or</h5>
+			</Col>
 			{x}
 		</>)}
 	</Row>
-	<h3>If your bathroom has more corners than fingers on one hand or specialty fixtures, get in touch!</h3>
+	<p>If your bathroom has more corners than fingers on your hands or specialty fixtures, we'll in touch later on to talk options!</p>
+	<Row className='justify-content-center'>
+		<Col xs='12' sm='auto'>
+			<Button color='primary' className='w-100'>
+				Continue To Quote!
+	</Button>
+		</Col>
+	</Row>
 </>
