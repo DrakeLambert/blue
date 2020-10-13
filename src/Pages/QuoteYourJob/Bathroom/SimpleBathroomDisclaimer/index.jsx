cf3cbfb934f5dc2e2e80a134c725c785a8851744
DrakeLambert <drake.askme2@gmail.com>
2020-10-12 21:44:25 -0500

    refactor out bathroom type disclaimer

---
+++ src/Pages/QuoteYourJob/Bathroom/SimpleBathroomDisclaimer/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import type1 from './type-1.svg'
+import type2 from './type-2.svg'
+import type3 from './type-3.svg'
+import { Button, Col, Row, Image } from 'react-bootstrap'
+
+const types = [type1, type2, type3]
+	.map((type, i) => ({ className }) => <Image src={type} alt={`bathroom type ${i}`} className={className} rounded />)
+
+export default () => <>
+	<h4>Just a heads up!</h4>
+	<p>We renovate more traditional bathrooms. Like this...</p>
+	<Row className='justify-content-between mb-3 mx-0'>
+		{types.map((TypeImage, i) =>
+			<Col xs={5} sm={3} key={i} className={(i < 2 ? '' : 'd-none d-sm-block ') + 'px-0'}>
+				<TypeImage className='w-100' />
+			</Col>
+		).reduce((xs, x, i) => <>
+			{xs}
+			<Col xs='auto' className={(i < 2 ? '' : 'd-none d-sm-block ') + 'align-self-center px-0'}>
+				<h5>or</h5>
+			</Col>
+			{x}
+		</>)}
+	</Row>
+	<p>If your bathroom has more corners than fingers on your hands or specialty fixtures, we'll in touch later on to talk options!</p>
+	<Row>
+		<Col xs='12' sm='auto'>
+			<Button color='primary' className='w-100'>
+				Continue To Quote!
+			</Button>
+		</Col>
+	</Row>
+</>
--- src/Pages/QuoteYourJob/Bathroom/index.jsx
+++ src/Pages/QuoteYourJob/Bathroom/index.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
-import typeImages from './TypeImages'
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom'
+import { routes } from '../..'
+import QuoteForm from './QuoteForm'
 
-export default () => <>
-	<h4>Just a heads up!</h4>
-	<p>We renovate more traditional bathrooms. Like this...</p>
-	<Row className='justify-content-around mb-3 mx-n0'>
-		{typeImages.map((TypeImage, i) =>
-			<Col xs={5} sm={3} key={i} className={(i < 2 ? '' : 'd-none d-sm-block ') + 'px-0'}>
-				<TypeImage className='w-100' />
-			</Col>
-		).reduce((xs, x, i) => <>
-			{xs}
-			<Col xs='auto' className={(i < 2 ? '' : 'd-none d-sm-block ') + 'align-self-center px-0'}>
-				<h5>or</h5>
-			</Col>
-			{x}
-		</>)}
-	</Row>
-	<p>If your bathroom has more corners than fingers on your hands or specialty fixtures, we'll in touch later on to talk options!</p>
-	<Row className='justify-content-center'>
-		<Col xs='12' sm='auto'>
-			<Button color='primary' className='w-100'>
-				Continue To Quote!
-	</Button>
-		</Col>
-	</Row>
-</>
+export default () => {
+	const routeMatch = useRouteMatch()
+
+	return <Switch>
+		<Route path={`${routeMatch.path}`} exact component={QuoteForm} />
+		<Route path='/'>
+			<Redirect to={routes.chooseJobType} />
+		</Route>
+	</Switch>
+}
