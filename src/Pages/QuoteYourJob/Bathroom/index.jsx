66c83a2d87cbf993e760a9090cd2a21f3a944f33
DrakeLambert <drake.askme2@gmail.com>
2020-10-19 22:11:38 -0500

    simplify bathroom quote component

--- src/Pages/QuoteYourJob/Bathroom/index.jsx
+++ src/Pages/QuoteYourJob/Bathroom/index.jsx
@@ -1,15 +1,4 @@
 import React from 'react'
-import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom'
-import { routes } from '../..'
 import QuoteForm from './QuoteForm'
 
-export default () => {
-	const routeMatch = useRouteMatch()
-
-	return <Switch>
-		<Route path={`${routeMatch.path}`} exact component={QuoteForm} />
-		<Route path='/'>
-			<Redirect to={routes.chooseJobType} />
-		</Route>
-	</Switch>
-}
+export default ({ onQuote }) => <QuoteForm onQuote={onQuote} />
