eaaa6a2a6a3220fcaa05b7bb2299e7900f5fa0b0
Drake Lambert <17054016+DrakeLambert@users.noreply.github.com>
2020-11-05 21:10:46 -0600

    return no content from http func

--- api/HttpExample.cs
+++ api/HttpExample.cs
@@ -1,11 +1,9 @@
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Api
 {
@@ -18,17 +16,7 @@ namespace Api
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = request.Query["name"];
-
-            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name     ??= data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new NoContentResult();
         }
     }
 }
