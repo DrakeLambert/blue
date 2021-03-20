c03e8937c7a715e2e582876767c82f42f606c487
Drake Lambert <17054016+DrakeLambert@users.noreply.github.com>
2021-03-20 15:47:07 -0500

    enable client side route tracking

--- app/src/analytics.js
+++ app/src/analytics.js
@@ -2,8 +2,8 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 const appInsights = new ApplicationInsights({
   config: {
-    instrumentationKey: '7622f0f2-a7a5-4159-b23c-7c3b54947322'
-    /* ...Other Configuration Options... */
+    instrumentationKey: '7622f0f2-a7a5-4159-b23c-7c3b54947322',
+    enableAutoRouteTracking: true
   }
 })
 appInsights.loadAppInsights()
